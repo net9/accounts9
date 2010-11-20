29cbebaedb193630ff1d2cea2f180875e856838c
Wyverald <wyverald@gmail.com>
2010-11-20 17:41:45 +0800

    Changed to using LESS.

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(express.cookieDecoder());
   app.use(express.session());
+  app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
   app.use(app.router);
   app.use(express.staticProvider(__dirname + '/public'));
 });
