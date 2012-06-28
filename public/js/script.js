36df88918efa5023c71834f9563e92c4592d3328
Brian <brian14708@gmail.com>
2012-06-28 20:52:22 +0800

    Updated groups view

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
+
     if ($("#group").length) {
         $("#group ul").hide();
         $("#group li").click(function(e) {
             if (e.target.className === 'node') {
                 var s = $(this);
-                if (s.children('ul').slideDown().length) {
+                if (s.hasClass('show')) {
+                    s.find('ul').slideUp();
+                    s.find('li').removeClass('show');
+                    s.removeClass('show');
+                } else if (s.children('ul').slideDown().length) {
                     s.addClass('show');
                 }
+                window.location.hash = s.attr('id');
+                return false;
             }
+            return true;
         });
-        $("#group > ul").show().children().addClass('show').children('ul').show();
+        $("#group > ul").show();
+        var elem = $(window.location.hash || "#group > ul > li");
+        elem.parentsUntil('#group', 'li').addClass('show').children('ul').show();
+        elem.children('div.node').click();
     }
 });
