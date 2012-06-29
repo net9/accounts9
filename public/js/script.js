52f146f88a7c7b91e14f73e8369765c7ac542b0f
Brian <brian14708@gmail.com>
2012-06-29 18:28:05 +0800

    Improved groups view

--- public/js/script.js
+++ public/js/script.js
@@ -2,29 +2,34 @@ $(document).ready(function() {
     "use strict";
 
     if (document.getElementById("group")) {
-        var lastOpened = '';
+        var lastOpened = window.location.hash.substring(1);
         $("#group li").click(function(e) {
-            if (e.target.className === 'node') {
+            if (e.target.className === "node") {
                 var s = $(this);
-                if (s.hasClass('show')) {
-                    s.find('ul').slideUp();
-                    s.find('li').removeClass('show');
-                    s.removeClass('show');
-                } else if (s.children('ul').slideDown().length) {
-                    s.addClass('show');
+                if (s.hasClass("show")) {
+                    s.find("ul").slideUp();
+                    s.find("li").removeClass("show");
+                    s.removeClass("show");
+                } else if (s.children("ul").slideDown().length) {
+                    s.addClass("show");
                 }
-                lastOpened = s.attr('id');
+                lastOpened = s.attr("id");
                 return false;
-            } else if (e.target.tagName === 'A') {
+            } else if (e.target.tagName === "A") {
+                $(window).unbind("hashchange");
                 window.location.hash = lastOpened;
             }
             return true;
         });
 
-        $("#group > ul ul").hide();
-        var elem = $(window.location.hash || "#group > ul > li");
-        elem.parentsUntil("#group", "li").addClass('show').children('ul').show();
-        elem.children("div.node").click();
+        var init = function() {
+            $("#group li").removeClass("show").children("ul").hide();
+            var elem = $(window.location.hash || "#group > ul > li");
+            elem.parentsUntil("#group", "li").addClass("show").children("ul").show();
+            elem.children("div.node").click();
+        }
+        $(window).bind("hashchange", init);
+        init();
     }
 
 });
